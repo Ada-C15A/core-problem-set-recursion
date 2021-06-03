1de8a3bdb7949bb2b7de0d48cf573d66f6cdd752
janicelichtman <jalichtman@gmail.com>
2021-06-03 12:08:01 -0700

    part-2 solutions added

--- part-1.py
+++ part-1.py
@@ -28,14 +28,25 @@ def bunny(count):
 
 
 # is_nested_parens
+# Original solution
 def is_nested_parens(parens):
     if not parens:
         return True
     return False if not (parens[0] == '(' and parens[-1] == ')') else is_nested_parens(parens[1:-1])
+
 '''
-I'm curious about the solution for the Challenge. I don't really understand the hint. 
+I'm curious about the solution for the Challenge, but not sure I fully understand what the hint is suggesting. 
     It said: For an additional challenge, implement is_nested_parens 
     without creating new strings in the process of solving the problem.
     HINT said: Sometimes we create extra helper functions that can store more information 
     in their parameters than we might use in the main solution function.
 '''
+# Potential solution for the challenge, but may have misunderstood
+def check_set(parens_pair):
+    return parens_pair == ("(", ")")
+    
+def is_nested_parens(parens):
+    if not parens:
+        return True
+    
+    return False if not check_set((parens[0], parens[-1])) else is_nested_parens(parens[1:-1])
--- part-2.py
+++ part-2.py
@@ -4,13 +4,44 @@
 # the appropriate comment.
 
 # search
-
+def search(array, value):
+    if not array: 
+        return False
+    return (array[0] == value or search(array[1:], value))
 
 
 # is_palindrome
-
-
+def is_palindrome(word):
+    if len(word) in {0,1}:
+        return True
+    return (word[0] == word[-1] and is_palindrome(word[1:-1]))
+'''
+I'm curious about the challenge, but I don't really understand where in the solution 
+in Learn (or in mine) new strings were being created.
+Challenge says: 
+For an additional challenge, implement is_palindrome without creating 
+new strings in the process of solving the problem.
+'''
 
 # digit_match
+# 2 solutiomns:
+# My original solution - it works, but changes the requested methhod signature
+def digit_match(num1, num2, count = 0):
+    if num1//10 == 0 or num2//10 == 0:
+        if num1%10 == num2%10:
+            count += 1
+        return count 
 
+    if num1%10 == num2%10:
+        count += 1
+    return digit_match(num1//10, num2//10, count)
+
+# Improved solution
+def digit_match(num1, num2):
+    if num1//10 == 0 or num2//10 == 0:
+        return 1 if num1%10 == num2%10 else 0
+
+    if num1%10 == num2%10:
+        return 1 + digit_match(num1//10, num2//10)
+    return digit_match(num1//10, num2//10)
 
