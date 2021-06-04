d0754e382ba9455b0a7eb31257984a13b8b88eb5
Cloudy Lopez <CloudyLopez@icloud.com>
2021-06-04 01:09:03 -0700

    questions completed - ish

--- part-1.py
+++ part-1.py
@@ -4,17 +4,37 @@
 # the appropriate comment.
 
 # factorial
+def factorial(n):
+    if not n:
+        return False
+    if n==1 or n==0:
+        return n
+    else:
+        facty = factorial(n-1)
+        result = n*facty
+        return result
 
-
-
-# reverse
-
-
+def reverse(string):
+    if len(string) == 0:
+        return 0
+    else:
+        return reverse(string[1:]) + string[0]
+reversed = str(input("Enter the string to be reversed: "))
+print(reverse(reversed))
 
 # bunny
-
+def bunny(count):
+    bunny = count * 2
+    return bunny
+# bunny q was n/a
 
 
 # is_nested_parens
+def is_nested_parens(thing):
+    if not thing:
+        return
+    is_nested_parens(thing[thing.find('(', 1):thing.rfind(')', 0, len(thing)-1)+1])
+    print(thing)
+
 
 
