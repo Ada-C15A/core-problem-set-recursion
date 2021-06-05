2698588792f0ffdf8611763cf2920a40ab739b1d
kaylaecker <kayjaysrainbows@gmail.com>
2021-06-04 17:09:14 -0700

    submits recursive solutions

--- part-1.py
+++ part-1.py
@@ -5,16 +5,41 @@
 
 # factorial
 
+def factorial(n):
+    if n == 0:
+        return 1
+    if n < 0:
+        raise ValueError("Value cannot be less than 0")
+    
+    return n * factorial(n-1)
 
 
 # reverse
 
+def reverse(text):
+    if len(text) == 0:
+        return ""
+    if len(text) == 1:
+        return text
+    
+    return text[-1] + reverse(text[:-1])
 
 
 # bunny
 
+def bunny(count):
+    if count == 0:
+        return 0
+    
+    return 2 + bunny(count-1)
 
 
 # is_nested_parens
 
+def is_nested_parens(parens):
+    if parens == "":
+        return True
+    if parens[0] != "(" or len(parens) == 1:
+        return False
 
+    return is_nested_parens(parens[1:-1])
--- part-2.py
+++ part-2.py
@@ -5,12 +5,44 @@
 
 # search
 
+def search(array, query):
+    if not array: 
+        return False
+    if array[-1] == query:
+        return True
+    
+    return search(array[:-1], query)
 
 
 # is_palindrome
 
+def is_palindrome(string):
+    if len(string) == 0:
+        return ""
+    if len(string) == 1:
+        return string
+    
+    if string[0] != string[-1]:
+        return False
+    
+    return is_palindrome(string[1:-1])
 
 
 # digit_match
 
-
+def digit_match(apples, oranges):
+    # if not apples or not oranges:
+        # return 0
+        # Why does this check need to be done after it is turned
+        # into a string? can't do it here on like 2... 
+    string_apple = str(apples)
+    string_oranges = str(oranges)
+    
+    if not string_apple or not string_oranges:
+        return 0
+    
+    if string_apple[-1] == string_oranges[-1]:
+        return 1 + digit_match(string_apple[:-1], string_oranges[:-1])
+    
+    return digit_match(string_apple[:-1], string_oranges[:-1])
+    
