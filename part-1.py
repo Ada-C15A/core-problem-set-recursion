f426d36de278a8ce24e118f498d3fe9af879827d
Dena Burd <denaburd@gmail.com>
2021-06-04 17:53:40 -0700

    Adds answers for problem sets 1 and 2

--- part-1.py
+++ part-1.py
@@ -4,17 +4,43 @@
 # the appropriate comment.
 
 # factorial
-
+def factorial(n):
+    if n < 0:
+        raise ValueError
+    if n <= 1:
+        return 1
+    else:
+        return n * factorial(n-1)
 
 
 # reverse
-
+def reverse(word):
+    if word == "":
+        return word
+    else:
+        return reverse(word[1:])+word[0]
 
 
 # bunny
-
+def bunny(count):
+    if count == 0:
+        return 0
+    elif count == 1:
+        return 2
+    else:
+        return bunny(count-1)+2
 
 
 # is_nested_parens
+def is_nested_parens(parens):
+    if len(parens) == 0:
+        return True
 
+    if len(parens) == 1:
+        return False
 
+    else:
+        if parens[0] == "(" and parens[-1] == ")":
+            return is_nested_parens(parens[1:-1])
+        else:
+            return False
--- part-2.py
+++ part-2.py
@@ -4,13 +4,35 @@
 # the appropriate comment.
 
 # search
-
+def search(arr, query):
+    if len(arr) == 0:
+        return False
+    if arr[0] == query or arr[-1] == query:
+        return True
+    else:
+        return search(arr[1:-1], query)
 
 
 # is_palindrome
-
+def is_palindrome(word):
+    if len(word) <= 1:
+        return True
+    if word[0] == word[-1]:
+        return is_palindrome(word[1:-1])
+    else:
+        return False
 
 
 # digit_match
 
+def digit_match(apples, oranges):
+    a_str = str(apples)
+    o_str = str(oranges)
 
+    if not a_str or not o_str:
+        return 0
+
+    if a_str[-1] == o_str[-1]:
+        return 1 + digit_match(a_str[:-1], o_str[:-1])
+    else:
+        return digit_match(a_str[:-1], o_str[:-1])
