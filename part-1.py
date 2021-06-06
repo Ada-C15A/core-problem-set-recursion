3d1cd88d2a4cf8133206c207d1c8ee5a1929dc6d
Cloudy Lopez <CloudyLopez@icloud.com>
2021-06-05 17:34:22 -0700

    updated some fn

--- part-1.py
+++ part-1.py
@@ -13,20 +13,19 @@ def factorial(n):
         facty = factorial(n-1)
         result = n*facty
         return result
+        
 
 def reverse(string):
     if len(string) == 0:
-        return 0
+        return ''
     else:
         return reverse(string[1:]) + string[0]
-reversed = str(input("Enter the string to be reversed: "))
-print(reverse(reversed))
+
 
 # bunny
 def bunny(count):
     bunny = count * 2
     return bunny
-# bunny q was n/a
 
 
 # is_nested_parens
@@ -34,7 +33,4 @@ def is_nested_parens(thing):
     if not thing:
         return
     is_nested_parens(thing[thing.find('(', 1):thing.rfind(')', 0, len(thing)-1)+1])
-    print(thing)
-
-
-
+    return(thing)
